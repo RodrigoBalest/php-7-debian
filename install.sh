47a3c0a1f9a4743edf5488d1cce575c366352537
RodrigoBalest <r.balest@gmail.com>
2018-07-26 10:15:26 -0300

    Add mod_fastcgi to Apache2

--- install.sh
+++ install.sh
@@ -13,6 +13,19 @@ cp conf/www.conf /usr/local/php7/etc/php-fpm.d/www.conf
 cp conf/php-fpm.conf /usr/local/php7/etc/php-fpm.conf
 cp conf/modules.ini /usr/local/php7/etc/conf.d/modules.ini
 
+# Add mod_fastcgi files
+cp conf/fastcgi.load /etc/apache2/mods-available/fastcgi.load
+cp conf/fastcgi.conf /etc/apache2/mods-available/fastcgi.conf
+cp conf/mod_fastcgi.so /usr/lib/apache2/modules/mod_fastcgi.so
+
+mkdir /var/lib/apache2
+mkdir /var/lib/apache2/fastcgi
+chgrp www-data /var/lib/apache2/fastcgi/
+chmod 775 /var/lib/apache2/fastcgi/
+
+a2enmod actions cgid fastcgi
+service apache2 restart
+
 # Add the init script
 cp conf/php7-fpm.init /etc/init.d/php7-fpm
 chmod +x /etc/init.d/php7-fpm
