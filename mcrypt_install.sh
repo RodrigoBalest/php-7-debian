1a8a49ac79f32ed0c32156dd5daeb978563a37db
RodrigoBalest <r.balest@gmail.com>
2018-07-26 10:33:32 -0300

    Create mcrypt_install.sh

---
+++ mcrypt_install.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+apt-get install -y php-pear libmcrypt-dev
+export PHP_PEAR_PHP_BIN=/usr/local/php7/bin/php
+
+echo "------"
+echo "ATENÇÃO: tecle ENTER para a pergunta 'libmcrypt prefix? [autodetect]'"
+echo "------"
+
+pecl install mcrypt-1.0.1
+
+echo "extension=mcrypt.so" > /usr/local/php7/etc/conf.d/mcrypt.ini
+
+# Remove os programas usados para instalar o mcrypt no PHP.
+x="$(dpkg --list | grep php | awk '/^ii/{ print $2}')"
+apt-get --purge remove $x
+
+/etc/init.d/php7-fpm restart
