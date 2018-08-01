26db99086ea5693c6b101c73dd61e75e0738047c
RodrigoBalest <r.balest@gmail.com>
2018-08-01 09:07:27 -0300

    Script error OK

--- clear.sh
+++ clear.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-$ x="$(dpkg --list | grep php | awk '/^ii/{ print $2}')"
-$ apt-get --purge remove $x
+x="$(dpkg --list | grep php | awk '/^ii/{ print $2}')"
+apt-get --purge remove $x
 
 unlink /etc/apache2/mods-enabled/php5.conf
 unlink /etc/apache2/mods-enabled/php5.load
